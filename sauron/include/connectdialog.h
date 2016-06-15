a42d69cb0a72f6e18ae5895ca80613d0364088ac
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2016-06-15 19:15:19 +0200

    Completado dialogo ConnectDialog

--- sauron/include/connectdialog.h
+++ sauron/include/connectdialog.h
@@ -3,11 +3,37 @@
 
 #include <QDialog>
 
+class QLabel;
+class QLineEdit;
+class QPushButton;
+
 class ConnectDialog : public QDialog
 {
     Q_OBJECT
 public:
     ConnectDialog(QWidget *parent = 0);
+    void done(int result);
+    QString domain() const;
+    QString username() const;
+    QString password() const;
+private slots:
+    void textChangedOnLineEdit();
+private:
+    void createWidgets();
+    void createConnections();
+
+    QLabel *_domainLabel;
+    QLineEdit *_domainLineEdit;
+    QLabel *_usernameLabel;
+    QLineEdit *_usernameLineEdit;
+    QLabel *_passwordLabel;
+    QLineEdit *_passwordLineEdit;
+    QPushButton *_connectButton;
+    QPushButton *_cancelButton;
+
+    QString _domain;
+    QString _username;
+    QString _password;
 };
 
 #endif // CONNECTDIALOG_H
--- sauron/src/connectdialog.cpp
+++ sauron/src/connectdialog.cpp
@@ -1,6 +1,100 @@
 #include "connectdialog.h"
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 ConnectDialog::ConnectDialog(QWidget *parent) : QDialog(parent)
 {
+    createWidgets();
+    createConnections();
+    setWindowTitle(tr("Conectar a CC"));
+    setFixedSize(sizeHint());
+}
 
+void ConnectDialog::done(int result)
+{
+    if(result) {
+        _domain = _domainLineEdit -> text();
+        _username = _usernameLineEdit -> text();
+        _password = _passwordLineEdit -> text();
+    }
+
+    QDialog::done(result);
+}
+
+QString ConnectDialog::domain() const
+{
+    return _domain;
+}
+
+QString ConnectDialog::username() const
+{
+    return _username;
+}
+
+QString ConnectDialog::password() const
+{
+    return _password;
+}
+
+void ConnectDialog::textChangedOnLineEdit()
+{
+    _connectButton -> setEnabled(!_domainLineEdit -> text().isEmpty() &&
+                                 !_usernameLineEdit -> text().isEmpty() &&
+                                 !_passwordLineEdit -> text().isEmpty());
+}
+
+void ConnectDialog::createWidgets()
+{
+    _domainLabel = new QLabel(tr("&Servidor:"));
+    _domainLineEdit = new QLineEdit;
+    _domainLabel -> setBuddy(_domainLineEdit);
+
+    _usernameLabel = new QLabel(tr("&Usuario:"));
+    _usernameLineEdit = new QLineEdit;
+    _usernameLabel -> setBuddy(_usernameLineEdit);
+
+    _passwordLabel = new QLabel(tr("&Password:"));
+    _passwordLineEdit = new QLineEdit;
+    _passwordLineEdit -> setEchoMode(QLineEdit::Password);
+    _passwordLabel -> setBuddy(_passwordLineEdit);
+
+    _connectButton = new QPushButton(tr("&Conectar"));
+    _connectButton -> setDefault(true);
+    _connectButton -> setEnabled(false);
+
+    _cancelButton = new QPushButton(tr("Cancel"));
+
+    QGridLayout *topLayout = new QGridLayout;
+
+    topLayout -> addWidget(_domainLabel, 0, 0, 1, 1);
+    topLayout -> addWidget(_domainLineEdit, 0, 1, 1, 1);
+    topLayout -> addWidget(_usernameLabel, 1, 0, 1, 1);
+    topLayout -> addWidget(_usernameLineEdit, 1, 1, 1, 1);
+    topLayout -> addWidget(_passwordLabel, 2, 0, 1, 1);
+    topLayout -> addWidget(_passwordLineEdit, 2, 1, 1, 1);
+
+    QHBoxLayout *bottomLayout = new QHBoxLayout;
+
+    bottomLayout -> addWidget(_connectButton);
+    bottomLayout -> addWidget(_cancelButton);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+
+    mainLayout -> addLayout(topLayout);
+    mainLayout -> addLayout(bottomLayout);
+
+    setLayout(mainLayout);
+}
+
+void ConnectDialog::createConnections()
+{
+    connect(_domainLineEdit, SIGNAL(textChanged(QString)), this, SLOT(textChangedOnLineEdit()));
+    connect(_usernameLineEdit, SIGNAL(textChanged(QString)), this, SLOT(textChangedOnLineEdit()));
+    connect(_passwordLineEdit, SIGNAL(textChanged(QString)), this, SLOT(textChangedOnLineEdit()));
+    connect(_connectButton, SIGNAL(clicked()), this, SLOT(accept()));
+    connect(_cancelButton, SIGNAL(clicked()), this, SLOT(reject()));
 }
