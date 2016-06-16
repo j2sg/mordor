324c7f95a3d16883036f075365f058ff0063348c
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2016-06-16 10:52:41 +0200

    Añadido metodo para configurar parametros de aplicacion

--- sauron/src/main.cpp
+++ sauron/src/main.cpp
@@ -1,13 +1,30 @@
 #include <QApplication>
+#include "global.h"
 #include "mainwindow.h"
+
+void setupApplication(QApplication *app);
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    setupApplication(&app);
+
     MainWindow mainWindow;
 
     mainWindow.show();
 
     return app.exec();
 
+}
+
+void setupApplication(QApplication *app)
+{
+    if(!app)
+        return;
+
+    app -> setOrganizationName(ORGANIZATION_NAME);
+    app -> setOrganizationDomain(ORGANIZATION_DOMAIN);
+    app -> setApplicationName(APPLICATION_NAME);
+    app -> setApplicationVersion(APPLICATION_VERSION);
 }
