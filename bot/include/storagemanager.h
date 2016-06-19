2550237dfc788068d38b82318943cf9bce051422
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2016-06-19 20:30:52 +0200

    Completada implementacion de gestor de almacenamiento del bot

--- bot/include/storagemanager.h
+++ bot/include/storagemanager.h
@@ -1,11 +1,16 @@
 #ifndef STORAGEMANAGER_H
 #define STORAGEMANAGER_H
 
+#include <QVariant>
+#include <QString>
+
 class StorageManager
 {
 public:
     static bool existsConfig();
     static bool createConfig(bool overwrite = false);
+    static QVariant readConfig(const QString& key);
+    static bool writeConfig(const QString& key, const QVariant& value);
 };
 
 #endif // STORAGEMANAGER_H
--- bot/src/storagemanager.cpp
+++ bot/src/storagemanager.cpp
@@ -16,6 +16,34 @@ bool StorageManager::createConfig(bool overwrite)
     QSettings setting("config.ini", QSettings::IniFormat);
 
     setting.setValue("Executed", true);
+    setting.setValue("jid",      "");
+    setting.setValue("password", "");
+    setting.setValue("nick",     "");
+
+    return true;
+}
+
+QVariant StorageManager::readConfig(const QString& key)
+{
+    if(!existsConfig())
+        return QVariant();
+
+    QSettings setting("config.ini", QSettings::IniFormat);
+
+    return setting.value(key);
+}
+
+bool StorageManager::writeConfig(const QString& key, const QVariant& value)
+{
+    if(!existsConfig())
+        return false;
+
+    QSettings setting("config.ini", QSettings::IniFormat);
+
+    if(!setting.contains(key))
+        return false;
+
+    setting.setValue(key, value);
 
     return true;
 }
